5cd5ab52a3f181d5b55bf2aba611f088a3259aea
s-wucl <s-wucl@53S500037948.it.bsd405.org>
2019-11-15 22:25:34 -0800

    Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,12 +4,22 @@
 
 package fracCalc;
 
+import java.util.Scanner;
+
 public class FracCalc {
 
     public static void main(String[] args)
     {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner s = new Scanner(System.in);
+    	System.out.print("Enter a fraction problem: ");
+    	String userResponse = s.nextLine();
+    	
+    	while (!s.equals("quit")) {
+    		System.out.println(produceAnswer(userResponse));
+    		System.out.print("Enter a fraction problem: ");
+        	userResponse = s.nextLine();
+    	}
+    	System.out.println("Program Ended.");
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -20,13 +30,34 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    public static String produceAnswer(String input){
+    	String temp = input
+    	String operand1 = temp.substring(0, temp.indexOf(' '));
+    	temp = temp.substring(temp.indexOf(' ') + 1);
+    	String operator = temp.substring(0, temp.indexOf(' '));
+    	temp = temp.substring(temp.indexOf(' ') + 1);
+    	String operand2 = temp;
+    	String temp = "";
+    	for(int i - 0; i < input.length(); i++) {
+    		if(input.charAt(i) == ' ') {
+    			if(operand1.equals("")) {
+    				operand1 = temp;
+    				temp = "";
+    			}else {
+    				operator = temp;
+    				temp = "";
+    			}
+    		}else if (i == input.length()- 1) {
+    			temp += input.charAt(i);
+    			operand2 = temp;
+    		}else {
+    			temp += input.charAt(i);
+    		}
+    	}
+    	Scanner s = new Scanner(input);
+    
+        return operand1;
     }
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
 
 }
